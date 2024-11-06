74c798b657393e6b5c2ffd9b9d366ad46724935f
Gustavo-Machado-Dutra <165703307+Gustavo-Machado-Dutra@users.noreply.github.com>
2024-11-06 16:12:23 -0300

    Prova

--- Prova.sql
+++ Prova.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE SistemaEscola
+CREATE DATABASE EscolaSistema
 
 CREATE TABLE Alunos(
-	ID_Aluno INT IDENTITY(1,1) PRIMARY KEY,
+	ID_Aluno INT IDENTITY PRIMARY KEY,
 	CPF VARCHAR(11),
 	NomeAluno VARCHAR(30),
 	SobrenomeAluno VARCHAR(80),
@@ -15,7 +15,7 @@ CREATE TABLE Alunos(
 );
 
 CREATE TABLE Boletim(
-	ID_Notas INT IDENTITY(1,1) PRIMARY KEY,
+	ID_Notas INT IDENTITY PRIMARY KEY,
 	ID_Aluno INT, 
 	NomeAluno VARCHAR(30),
 	SobrenomeAluno VARCHAR(80),
@@ -33,7 +33,7 @@ CREATE TABLE Boletim(
 );
 
 CREATE TABLE Responsaveis(
-	ID_Responsavel INT IDENTITY(1,1) PRIMARY KEY,
+	ID_Responsavel INT IDENTITY PRIMARY KEY,
 	CPF VARCHAR(11),
 	NomeResponsavel VARCHAR(30),
 	SobrenomeResponsavel VARCHAR(80),
@@ -48,23 +48,19 @@ CREATE TABLE Responsaveis(
 
 --INSERE VALORES NA TABELA ALUNOS
 INSERT INTO Alunos(CPF, NomeAluno, SobrenomeAluno, DataNascimento, Serie, MensalidadePreco, DataMatricula, ID_Notas, ID_Responsavel)
-VALUES (51592930293, 'Lucas', 'Santos de Lima', '2003/08/19', 'Primeiro E.M', 1200, '2024/01/13', 1, 1 )
-
-
+VALUES (51592930293, 'Lucas', 'Santos de Lima', '2003/08/19', 'Primeiro E.M', 1200, '2024/01/13', 1, 1 );
 
 --INSERE VALORES NA TABELA BOLETIM
 INSERT INTO Boletim(ID_Aluno, NomeAluno, SobrenomeAluno , Serie , Portugues , Matematica , Fisica , Quimica , Biologia , Ingles , Artes , EducacaoFisica , FaltasTotal)
-VALUES (1, 'Lucas', 'Santos de Lima', 'Primeiro E.M', 6.5, 8, 7, 6, 6, 9, 6, 7, 46)
+VALUES (1, 'Lucas', 'Santos de Lima', 'Primeiro E.M', 6.5, 8, 7, 6, 6, 9, 6, 7, 46);
 
 --INSERE VALORES NA TABELA RESPONSAVEIS
 INSERT INTO Responsaveis(CPF , NomeResponsavel ,SobrenomeResponsavel ,DataNascimento ,DataMatricula ,Renda,Telefone,ContatoEmergencia ,ID_Aluno 	)
-VALUES (83458592042, 'Marcelo', 'Melo Santos', '1993/07/23', '2023/01/18', 5500, '11949532892', '11992534942', 1)
+VALUES (83458592042, 'Marcelo', 'Melo Santos', '1993/07/23', '2023/01/18', 5500, '11949532892', '11992534942', 1);
 
 --INSERE VALORES NA TABELA ALUNOS
 INSERT INTO Alunos(CPF, NomeAluno, SobrenomeAluno, DataNascimento, Serie, MensalidadePreco, DataMatricula, ID_Notas, ID_Responsavel)
 VALUES (43592930293, 'Pedro', 'Santos', '2005/08/19', 'Primeiro E.M', 1200, '2023/01/13', 2, 2 );
-
-
 
 --INSERE VALORES NA TABELA BOLETIM
 INSERT INTO Boletim(ID_Aluno, NomeAluno, SobrenomeAluno , Serie , Portugues , Matematica , Fisica , Quimica , Biologia , Ingles , Artes , EducacaoFisica , FaltasTotal)
@@ -99,15 +95,10 @@ ADD CONSTRAINT FK_ID_Responsavel
 FOREIGN KEY (ID_Responsavel)
 REFERENCES Responsaveis(ID_Responsavel)
 
---ADICIONA A COLUNA ID_Aluno COMO ESTRANGEIRA NA TABELA RESPONSAVEIS
-ALTER TABLE Responsaveis
-ADD CONSTRAINT FK_ID_Aluno
-FOREIGN KEY (ID_Aluno)
-REFERENCES Alunos(ID_Aluno)
 
 --ADICIONA A COLUNA ID_Aluno COMO ESTRANGEIRA NA TABELA BOLETIM
 ALTER TABLE Boletim
-ADD CONSTRAINT FK2_ID_Aluno
+ADD CONSTRAINT FK_ID_Aluno
 FOREIGN KEY (ID_Aluno)
 REFERENCES Alunos(ID_Aluno)
 
@@ -152,7 +143,6 @@ FROM Responsaveis
 
 
 --FUNCTION
-
 --CONSULTA O TELEFONE DO RESPONSÁVEL DO ALUNO
 CREATE FUNCTION ConsultarTelefone (@Telefone INT)
 RETURNS TABLE
@@ -171,52 +161,12 @@ RETURN (
 
 
 SELECT *
-FROM dbo.ConsultarTelefone(3); 
+FROM dbo.ConsultarTelefone(2); --COLOCAR O ID DO ALUNO ENTRE PARENTESES PARA CONSULTAR O TELEFONE
 
 
 
-
---WINDOW FUNCTION
-
-IF EXISTS (SELECT 1 FROM SYS.objects WHERE TYPE = 'P' AND NAME = 'SP_ADD_ALUNO')
-	BEGIN
-		DROP PROCEDURE SP_ADD_ALUNO
-	END
-GO
-
-CREATE PROCEDURE SP_ADD_ALUNO
-    @CPF VARCHAR(11),
-    @NomeAluno VARCHAR(30),
-    @SobrenomeAluno VARCHAR(80),
-    @DataNascimento DATE,
-	@Serie VARCHAR(20),
-	@MensalidadePreco FLOAT,
-	@DataMatricula DATE, 
-	@DataDesmatricula DATE, 
-	@ID_Notas INT, 
-	@ID_Responsavel INT
-AS
-    INSERT INTO Alunos(CPF, NomeAluno, SobrenomeAluno, DataNascimento, Serie, MensalidadePreco, DataMatricula, DataDesmatricula, ID_Notas, ID_Responsavel)
-    VALUES (@CPF, @NomeAluno, @SobrenomeAluno, @DataNascimento, @Serie, @MensalidadePreco, @DataMatricula, @DataDesmatricula, @ID_Notas, @ID_Responsavel);
-GO
-
-EXEC SP_ADD_ALUNO
-    @CPF = 69385038513,
-    @NomeAluno = 'Pedro',
-    @SobrenomeAluno = 'Alcantra',
-	@DataNascimento = '2007/02/23',
-    @Serie = 'Terceiro E.M',
-	@MensalidadePreco = 1400,
-	@DataMatricula = '2022/01/12',
-	@DataDesmatricula = NULL,
-	@ID_Notas = 2,
-	@ID_Responsavel = 2
-
-SELECT *
-FROM Alunos	
 
 --VIEW
-
 --VER QUAIS ALUNOS FICARAM COM NOTA ABAIXO DE 6 EM PORTUGUES
 CREATE VIEW vw_notas_portugues AS
 SELECT 
@@ -228,3 +178,111 @@ WHERE Portugues < 6
 
 SELECT *
 FROM vw_notas_portugues
+
+
+
+
+--PROCEDURE
+--ADICIONA UM NOVO RESPONSAVEL NA TABELA RESPONSAVEIS
+IF EXISTS (SELECT 1 FROM SYS.objects WHERE TYPE = 'P' AND NAME = 'SP_ADD_RESPONSAVEL')
+BEGIN
+    DROP PROCEDURE SP_ADD_RESPONSAVEL
+END
+GO
+
+CREATE PROCEDURE SP_ADD_RESPONSAVEL
+    @CPF VARCHAR(11),
+    @NomeResponsavel VARCHAR(30),
+    @SobrenomeResponsavel VARCHAR(80),
+    @DataNascimento DATE,
+    @Renda FLOAT, -- Novo parâmetro Renda
+    @Telefone VARCHAR(15), -- Novo parâmetro Telefone
+    @ContatoEmergencia VARCHAR(100), -- Novo parâmetro ContatoEmergencia
+    @MensalidadePreco FLOAT,
+    @DataMatricula DATE, 
+    @DataDesmatricula DATE ,
+	@ID_Aluno INT
+AS
+    INSERT INTO Responsaveis(CPF, NomeResponsavel, SobrenomeResponsavel, DataNascimento, Renda, Telefone, ContatoEmergencia, DataMatricula, DataDesmatricula, ID_Aluno)
+    VALUES (@CPF, @NomeResponsavel, @SobrenomeResponsavel, @DataNascimento, @Renda, @Telefone, @ContatoEmergencia, @DataMatricula, @DataDesmatricula, @ID_Aluno);
+GO
+
+EXEC SP_ADD_RESPONSAVEL
+    @CPF = '69385038513',
+    @NomeResponsavel = 'Pedro',
+    @SobrenomeResponsavel = 'Alcantra',
+    @DataNascimento = '2007/02/23',
+    @Renda = 5000, 
+    @Telefone = '11987654321',
+    @ContatoEmergencia = '11987659021', 
+    @MensalidadePreco = 1400,
+    @DataMatricula = '2022/01/12',
+    @DataDesmatricula = NULL,
+	@ID_Aluno = 4
+
+
+
+
+--CTE
+--CONSULTA O NOME DO RESPONSAVEL DO ALUNO, NOME DO ALUNO TELEFONE E CONTATO DE EMERGENCIA
+WITH ConsultarResponsavel AS (
+	SELECT
+	NomeResponsavel,
+	NomeAluno,
+	Telefone,
+	ContatoEmergencia
+	FROM Alunos
+	INNER JOIN
+	Responsaveis 
+	ON Alunos.ID_Aluno = Responsaveis.ID_Aluno
+) 
+SELECT *
+FROM ConsultarResponsavel
+
+
+
+
+
+--WINDOW FUNCTION
+--CALCULA A MÉDIA DAS NOTAS DE QUIMICA ENTRE OS ALUNOS DO PRIMEIRO E.M
+SELECT 
+    ROUND(AVG(Quimica) OVER (ORDER BY Quimica), 2) AS 'Média de Quimica' --ROUD ARREDONDA O NÚMERO DE CASAS DECIMAIS PARA 2 E O AVG CALCULA A MÉDIA COM BASE NA MATÉRIA QUIMICA
+FROM Boletim
+WHERE Serie = 'Primeiro E.M';
+
+
+
+--LOOP
+--CONFERE QUAIS ALUNOS FORAM APROVADOS NA MATÉRIA QUIMICA
+DECLARE @ID_Aluno INT;
+DECLARE @Nota FLOAT;
+DECLARE @NomeAluno VARCHAR(30);
+DECLARE @StatusAluno VARCHAR(20);
+
+SET @ID_Aluno = 1;
+
+WHILE (@ID_Aluno <= 3)
+BEGIN
+	SELECT 
+		@NomeAluno = Alunos.NomeAluno,
+		@Nota = Quimica
+	FROM Alunos
+	INNER JOIN 
+		Boletim
+	ON 
+		Alunos.ID_Aluno = Boletim.ID_Aluno
+	WHERE Alunos.ID_Aluno = @ID_Aluno;
+
+	IF @Nota >= 6 
+	BEGIN
+		SET @StatusAluno = 'Aprovado';
+	END
+	ELSE
+	BEGIN 
+		SET @StatusAluno = 'Recuperação';
+	END
+
+	PRINT @NomeAluno + ' - ' + @StatusAluno;
+
+	SET @ID_Aluno = @ID_Aluno + 1;
+END;
